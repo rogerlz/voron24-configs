98d4dbaa8336247dbdb42f60c5368e3120f6a718
Rogerio Goncalves <rogerlz@gmail.com>
2022-07-26 00:41:16 +0100

    auto backup 26-07-2022

--- scripts/flashboards.sh
+++ scripts/flashboards.sh
@@ -12,3 +12,9 @@ make flash
 make clean KCONFIG_CONFIG=config.octopus
 make KCONFIG_CONFIG=config.octopus
 make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_lpc1768_08A0000B610039AF9AF4F45BC02000F5-if00
+
+# sht36
+
+# /opt/klipper-venv/bin/python /opt/CanBoot/scripts/flash_can.py -i can0 -f out/klipper.bin -u 11ab3fae057c
+# /opt/klipper-venv/bin/python /opt/klipper/scripts/canbus_query.py can0
+
